1ae98ad90dd0778cd4ca80a61414ab787afa18cf
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-16 14:20:41 +0100

    Build from feature branch, merge into master in release job

--- jenkins/seed/seedDsl.groovy
+++ jenkins/seed/seedDsl.groovy
@@ -1,22 +1,64 @@
-job('submod-red') {
-  description('Build the submod-red project')
+repo = 'submod-red'
+
+job("${repo}-build") {
+  description('Check out feature branch and build source code')
 
   scm {
-    github('praqma-test/submod-red',
-      { scm ->
-        scm / 'extensions' / 'hudson.plugins.git.extensions.impl.SubmoduleOption' {
-          disableSubmodules false
-          recursiveSubmodules true
-          trackingSubmodules false
+    git('https://github.com/praqma-test/submod-red.git') {
+      branch 'feature/1'
+      extensions {
+        submoduleOptions {
+          disable false
+          recursive true
+          tracking false
         }
       }
-    )
+    }
+  }
+
+  steps {
+    shell('./build.sh')
+  }
+
+  publishers {
+    archiveArtifacts {
+      pattern('main')
+    }
+  }
+}
+
+job("${repo}-test") {
+  description('Run tests on the artifact from the build job')
+
+  steps {
+    copyArtifacts("${repo}-build") {
+      includePatterns('main')
+    }
+
+    shell('./main $BUILD_NUMBER')
+  }
+}
+
+job("${repo}-release") {
+  description('Check out master and merge feature branch into master')
+
+  scm {
+    git('https://github.com/praqma-test/submod-red.git') {
+      branch 'master'
+      extensions {
+        submoduleOptions {
+          disable false
+          recursive true
+          tracking false
+        }
+      }
+    }
   }
 
   steps {
     shell('''\
-    ./build.sh
-    ./main 3
+    git pull origin feature/1
+    git push
     '''.stripIndent())
   }
 }
