18d7a60f02597f569ae61659f14ee96a6faf6e66
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-21 09:56:08 +0100

    Use test.sh in lower-letters repo

--- jenkins/seed/seedDsl.groovy
+++ jenkins/seed/seedDsl.groovy
@@ -31,18 +31,7 @@ job("lower-letters-test") {
   }
 
   steps {
-    shell('''\
-    #!/bin/bash -x
-    cat text.txt | grep [A-Z]
-    if [ $? -eq 0 ]
-    then
-      echo Failure: Found uppercase letters
-      exit 1
-    else
-      echo Test passed
-      exit 0
-    fi
-    '''.stripIndent())
+    shell('./test.sh')
   }
 
 }
