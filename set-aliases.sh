2f6d3b2c26e40f2357ab4513898c134f6fe20129
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-29 11:25:41 +0200

    Update pullall alias to use one branch name

--- set-aliases.sh
+++ set-aliases.sh
@@ -4,11 +4,13 @@
 
 # Pull all remote changes to the superproject
 # and all submodules.
+# Use 'git rev-parse' to get the branch name, such that the same branch is pulled
+# for both the superproject and all submodules.
 # Gotcha: 'git submodule update' will silently overwrite any local changes
-# within a submodule.
+# within a submodule. See https://git.wiki.kernel.org/index.php/GitSubmoduleTutorial
 git config alias.pullall "!f() { git pull && \
     git submodule update --init --recursive && \
-    git submodule foreach git pull origin master; \
+    git submodule foreach git pull origin `git rev-parse --abbrev-ref HEAD`; \
   }; f"
 
 # Show the status of the superproject and all submodules.
