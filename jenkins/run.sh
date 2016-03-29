2c533c93d986c4dadc76fd6736e517ad8be0c346
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2016-03-29 14:17:52 +0200

    Clean up Jenkins

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -3,5 +3,7 @@ docker stop jenkins
 docker rm -v jenkins
 docker run -d -p 8080:8080 -p 50000:50000 \
               --name jenkins praqma/submod-env-jenkins
-echo Following logs, Ctrl+C to quit
+echo ==================================
+echo Following the logs, press Ctrl+C to quit
+echo ==================================
 docker logs -f jenkins
